9a35c9c60bd54df13fda6a75ccedbb2d7fc5fee7
Пользователь <genius.s.1799@gmail.com>
2021-04-28 12:20:29 +0500

    feat: creat register modal

--- components/Header.vue
+++ components/Header.vue
@@ -1,61 +1,158 @@
 <template>
-  <header class="header">
-    <div class="container mx-auto px-4 flex justify-between items-center">
-      <div class="header_logo">
-        <img src="@/assets/img/home/logo.svg" alt="mathrix-logo" />
-      </div>
+  <div>
+    <header class="header">
       <div
-        class="header__action grid gap-4 grid-cols-2 grid-rows-2 sm:grid-cols-3 lg:grid-cols-5 lg:grid-rows-1"
+        class="container mx-auto px-4 xl:px-0 flex justify-between items-center"
       >
-        <el-input
-          class="header__search col-start-1 row-start-2 sm:col-start-2 sm:col-span-2 lg:col-start-1 lg:row-start-1"
-          placeholder="Find what interests you"
-          prefix-icon="el-icon-search"
-          v-model="search"
-        >
-        </el-input>
-        <div
-          class="header__select col-start-2 row-start-2 sm:col-start-1 sm:row-start-1 lg:col-start-3"
-        >
-          <el-select v-model="value" placeholder="Terminal ES">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
-            </el-option>
-          </el-select>
+        <div class="header_logo">
+          <img src="@/assets/img/home/logo.svg" alt="mathrix-logo" />
         </div>
+        <div
+          class="header__action grid gap-4 grid-cols-2 grid-rows-2 sm:grid-cols-3 lg:grid-cols-5 lg:grid-rows-1"
+        >
+          <el-input
+            class="header__search col-start-1 row-start-2 sm:col-start-2 sm:col-span-2 lg:col-start-1 lg:row-start-1"
+            placeholder="Find what interests you"
+            prefix-icon="el-icon-search"
+            v-model="search"
+          >
+          </el-input>
+          <div
+            class="header__select col-start-2 row-start-2 sm:col-start-1 sm:row-start-1 lg:col-start-3"
+          >
+            <el-select v-model="value" placeholder="Terminal ES">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
 
-        <button class="header__btn login-btn">Log In</button>
-        <button class="m-btn-primary register-btn">Registration</button>
+          <button class="header__btn login-btn">Log In</button>
+          <button class="m-btn-primary register-btn" @click="openRegisterModal">
+            Registration
+          </button>
+        </div>
       </div>
+    </header>
+    <div class="register__modal">
+      <el-dialog :visible.sync="dialogVisible" class="register__modal">
+        <div class="register__modal-logo">
+          <img src="@/assets/img/logo/mathrix-logo.svg" alt="mathrix-logo" />
+        </div>
+        <h2 class="register__modal-title modal-title">Sign up</h2>
+        <p class="register__modal-subtitle modal-subtitle">
+          Have an account? <span @click="openLoginModal">Log in</span>
+        </p>
+        <el-form
+          label-position="top"
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="120px"
+          class="demo-ruleForm"
+        >
+          <el-form-item prop="email" label="Email" :rules="rules">
+            <el-input v-model="ruleForm.email"></el-input>
+          </el-form-item>
+
+          <el-form-item label="Password" prop="pass">
+            <el-input
+              type="password"
+              v-model="ruleForm.pass"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+
+          <button
+            class="m-btn-primary register__modal-btn"
+            @click="submitForm('ruleForm')"
+          >
+            Registration
+          </button>
+          <div class="register__modal-devider">or</div>
+          <button class="register__modal-btn">
+            <span>Sign up with Facebook</span>
+          </button>
+          <button class="register__modal-btn">
+            <span>Sign up with Google</span>
+          </button>
+        </el-form>
+      </el-dialog>
     </div>
-  </header>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please input the password"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
     return {
       options: [
         {
           value: "Terminal ES",
-          label: "Terminal ES",
+          label: "Terminal ES"
         },
         {
           value: "Terminal L",
-          label: "Terminal L",
+          label: "Terminal L"
         },
         {
           value: "Terminal S",
-          label: "Terminal S",
-        },
+          label: "Terminal S"
+        }
       ],
       value: "Terminal ES",
       search: "",
+      dialogVisible: false,
+      ruleForm: {
+        email: "",
+        pass: ""
+      },
+      rules: {
+        email: [
+          {
+            required: true,
+            message: "Please input email address",
+            trigger: "blur"
+          },
+          {
+            type: "email",
+            message: "Please input correct email address",
+            trigger: ["blur", "change"]
+          }
+        ],
+        pass: [{ validator: validatePass, trigger: "blur" }]
+      }
     };
   },
+  methods: {
+    openRegisterModal() {
+      this.dialogVisible = true;
+    },
+    openLoginModal() {},
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
 };
 </script>
