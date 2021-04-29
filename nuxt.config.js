d221defebe8eb5bbb73891c81cd05a87d92fa005
Пользователь <genius.s.1799@gmail.com>
2021-04-29 14:25:11 +0500

    feat: add @voerro-tagsinput plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "@/plugins/element-ui",
-    { src: "@/plugins/vue-awesome-swiper", mode: "client" }
+    { src: "@/plugins/vue-awesome-swiper", mode: "client" },
+    { src: "@/plugins/voerro-tags-input", mode: "client" }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
