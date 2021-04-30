d1ffe9fff40efe1c893053282f41c4ebc668ecc7
WINDOWS 10 <genius.s.1799@gmail.com>
2021-04-30 21:24:05 +0500

    fix: fix error of key value in  UseApp

--- components/UseApp.vue
+++ components/UseApp.vue
@@ -1,33 +1,33 @@
 <template>
   <section class="app relative">
-      <div class="container mx-auto text-center relative z-10">
-        <h2 class="app__title section-title">Use the app to review anywhere</h2>
-        <div class="flex justify-center">
-          <a class="app__btn" href="#">
-            <img
-              src="@/assets/img/icons/play-market-icon.svg"
-              alt="play-market"
-              class="app__bnt"
-            />
-          </a>
-          <a class="app__btn" href="#">
-            <img
-              src="@/assets/img/icons/app-store-icon.svg"
-              alt="app-store"
-              class="app__bnt"
-            />
-          </a>
-        </div>
+    <div class="container mx-auto text-center relative z-10">
+      <h2 class="app__title section-title">Use the app to review anywhere</h2>
+      <div class="flex justify-center">
+        <a class="app__btn" href="#">
+          <img
+            src="@/assets/img/icons/play-market-icon.svg"
+            alt="play-market"
+            class="app__bnt"
+          />
+        </a>
+        <a class="app__btn" href="#">
+          <img
+            src="@/assets/img/icons/app-store-icon.svg"
+            alt="app-store"
+            class="app__bnt"
+          />
+        </a>
       </div>
-      <div class="app__patterns">
-        <img
-          :src="appPattern.img"
-          alt="pattern"
-          v-for="appPattern of appPatterns"
-          :key="appPattern"
-        />
-      </div>
-    </section>
+    </div>
+    <div class="app__patterns">
+      <img
+        :src="appPattern.img"
+        alt="pattern"
+        v-for="(appPattern, index) of appPatterns"
+        :key="index"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
@@ -41,5 +41,5 @@ export default {
       ],
     };
   },
-}
+};
 </script>
