a5e768e5a6e90dd6d96c4a217382725b0d418250
Пользователь <genius.s.1799@gmail.com>
2021-05-01 14:22:04 +0500

    feat: implement dashboard motivation tab

--- pages/index.vue
+++ pages/index.vue
@@ -1,90 +1,151 @@
 <template>
-  <div class="dashboard container mx-auto">
-    <div class="dashboard__header flex justify-between">
-      <h2 class="dashboard__title page-title">Welcome, {{ user.username }}!</h2>
-      <div class="dashboard__activity flex justify-between">
-        <p>
-          <span>{{ user.mins }}</span> mins viewed
-        </p>
-        <p>
-          <span>{{ user.activities }}</span> activities checked
-        </p>
+  <div class="dashboard">
+    <div class="container mx-auto">
+      <div class="dashboard__header flex justify-between">
+        <h2 class="dashboard__title page-title">
+          Welcome, {{ user.username }}!
+        </h2>
+        <div class="dashboard__activity flex justify-between">
+          <p class="mr-4">
+            <span class="mr-1">{{ user.mins }}</span> mins viewed
+          </p>
+          <p>
+            <span class="mr-1">{{ user.activities }}</span> activities checked
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="dashboard__tabs">
-      <el-tabs v-model="activeTab" :stretch="true">
-        <h3 class="courses__title">Your active course</h3>
-        <el-tab-pane label="Subjects" name="subjects">
-          <div class="courses__list grid grid-cols-3 gap-6">
-            <div
-              class="courses__item course relative"
-              v-for="(course, index) of courses"
-              :key="index"
-            >
-              <h4 class="course__name">{{ course.name }}</h4>
-              <img
-                class="course__pattern"
-                :src="course.pattern"
-                alt="course-patterns"
-              />
-              <div class="flex items-center">
-                <div class="course__progress mr-3">
-                  <el-progress
-                    type="circle"
-                    :percentage="course.percentage"
-                    color="#FF773C"
-                  ></el-progress>
+      <div class="dashboard__tabs">
+        <el-tabs v-model="activeTab" :stretch="true">
+          <el-tab-pane label="Subjects" name="subjects">
+            <h3 class="courses__title">Your active course</h3>
+            <div class="courses__list grid grid-cols-3 gap-6">
+              <div
+                class="courses__item course relative"
+                v-for="(course, index) of courses"
+                :key="index"
+              >
+                <h4 class="course__name">{{ course.name }}</h4>
+                <img
+                  class="course__pattern"
+                  :src="course.pattern"
+                  alt="course-patterns"
+                />
+                <div class="flex items-center">
+                  <div class="course__progress mr-3">
+                    <el-progress
+                      type="circle"
+                      :percentage="course.percentage"
+                      color="#FF773C"
+                    ></el-progress>
+                  </div>
+                  <div>
+                    <p class="course__topic">{{ course.topic }}</p>
+                    <nuxt-link :to="course.link">
+                      <p class="course__continue mt-1">
+                        Cointinue
+                        <el-icon class="el-icon-right pt-1 ml-1"></el-icon>
+                      </p>
+                    </nuxt-link>
+                  </div>
                 </div>
-                <div>
-                  <p class="course__topic">{{ course.topic }}</p>
-                  <nuxt-link :to="course.link">
-                    <p class="course__continue mt-1">
-                      Cointinue
-                      <el-icon class="el-icon-right pt-1 ml-1"></el-icon>
+                <div
+                  class="course__compalate complate grid grid-cols-2 grid-rows-2 items-center justify-center"
+                >
+                  <div
+                    class="complate__item flex flex-col justify-center items-center"
+                    v-for="(item, index) of complatedTasks"
+                    :key="index"
+                  >
+                    <p class="complate__title">
+                      {{ item.complated }}/{{ item.max }}
                     </p>
+                    <p class="complate__subtitle">videos checked</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subject">
+              <h3 class="subject__title step-title">Coose the subject</h3>
+              <div class="subject__items grid grid-cols-4 grid-rows-3">
+                <div v-for="(subject, index) of subjects" :key="index">
+                  <nuxt-link :to="subject.link">
+                    <div class="subject__item flex flex-col">
+                      <img
+                        :src="subject.img"
+                        alt="subject"
+                        class="subject__img"
+                      />
+                      <p class="subject__name">{{ subject.name }}</p>
+                    </div>
                   </nuxt-link>
                 </div>
               </div>
-              <div
-                class="course__compalate complate grid grid-cols-2 grid-rows-2 items-center justify-center"
-              >
-                <div
-                  class="complate__item flex flex-col justify-center items-center"
-                  v-for="(item, index) of complatedTasks"
-                  :key="index"
-                >
-                  <p class="complate__title">
-                    {{ item.complated }}/{{ item.max }}
-                  </p>
-                  <p class="complate__subtitle">videos checked</p>
+            </div>
+          </el-tab-pane>
+
+          <el-tab-pane label="Motivation" name="motivation">
+            <div class="flex motivation">
+              <aside class="motivation__sidebar sidebar">
+                <nav class="sidebar__menu">
+                  <ul class="sidebar__list flex flex-col">
+                    <li>
+                      <a class="sidebar__item _active" @click="sidebarActive">
+                        See all
+                      </a>
+                    </li>
+                    <li>
+                      <a class="sidebar__item" @click="sidebarActive">
+                        Access success
+                      </a>
+                    </li>
+                    <li>
+                      <a class="sidebar__item" @click="sidebarActive">
+                        Ask Methrix show
+                      </a>
+                    </li>
+                    <li>
+                      <a class="sidebar__item" @click="sidebarActive">
+                        Special issue
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </aside>
+              <div class="motivation__content flex flex-col">
+                <div class="motivation__success">
+                  <h3 class="motivation__title">Access success</h3>
+                  <div class="motivation__list grid grid-cols-2">
+                    <div
+                      class="motivation__item"
+                      v-for="(success, s) of successList"
+                      :key="s"
+                    >
+                      <img :src="success.img" alt="mathrix motivation" />
+                      <p>{{ success.title }}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="motivation__show">
+                  <h3 class="motivation__title">Ask Methrix show</h3>
+                  <div class="motivation__list grid grid-cols-2">
+                    <div
+                      class="motivation__item"
+                      v-for="(show, sh) of showList"
+                      :key="sh"
+                    >
+                      <img :src="show.img" alt="mathrix motivation" />
+                      <p>{{ show.title }}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </el-tab-pane>
 
-          <div class="subject">
-            <h3 class="subject__title step-title">Coose the subject</h3>
-            <div class="subject__items grid grid-cols-4 grid-rows-3">
-              <div v-for="(subject, index) of subjects" :key="index">
-                <nuxt-link :to="subject.link">
-                  <div class="subject__item flex flex-col">
-                    <img
-                      :src="subject.img"
-                      alt="subject"
-                      class="subject__img"
-                    />
-                    <p class="subject__name">{{ subject.name }}</p>
-                  </div>
-                </nuxt-link>
-              </div>
-            </div>
-          </div>
-        </el-tab-pane>
-        <el-tab-pane label="Motivation" name="motivation"
-          >Motivation</el-tab-pane
-        >
-        <el-tab-pane label="Training" name="training">Training</el-tab-pane>
-      </el-tabs>
+          <el-tab-pane label="Training" name="training">Training</el-tab-pane>
+        </el-tabs>
+      </div>
     </div>
     <use-app />
   </div>
@@ -98,7 +159,7 @@ export default {
     user: {
       username: "Callie",
       mins: 12,
-      activities: 40,
+      activities: 40
     },
     courses: [
       {
@@ -114,7 +175,7 @@ export default {
         flashcardComplated: 0,
         totolFlashcard: 10,
         examComplated: 1,
-        totolExam: 4,
+        totolExam: 4
       },
       {
         name: "History",
@@ -129,82 +190,135 @@ export default {
         flashcardComplated: 0,
         totolFlashcard: 10,
         examComplated: 1,
-        totolExam: 4,
-      },
+        totolExam: 4
+      }
     ],
     subjects: [
       {
         img: require("@/assets/img/subjects/maths.svg"),
         name: "Mathematics",
-        link: "/dashboard/maths",
+        link: "/dashboard/maths"
       },
       {
         img: require("@/assets/img/subjects/physical.svg"),
         name: "Physical",
-        link: "/dashboard/physical",
+        link: "/dashboard/physical"
       },
       {
         img: require("@/assets/img/subjects/svt.svg"),
         name: "SVT",
-        link: "/dashboard/svt",
+        link: "/dashboard/svt"
       },
       {
         img: require("@/assets/img/subjects/history.svg"),
         name: "History",
-        link: "/dashboard/history",
+        link: "/dashboard/history"
       },
       {
         img: require("@/assets/img/subjects/chemistry.svg"),
         name: "Chemistry",
-        link: "/dashboard/chemistry",
+        link: "/dashboard/chemistry"
       },
       {
         img: require("@/assets/img/subjects/numeric.svg"),
         name: "Numeric Science",
-        link: "/dashboard/numeric",
+        link: "/dashboard/numeric"
       },
       {
         img: require("@/assets/img/subjects/technology.svg"),
         name: "Technology",
-        link: "/dashboard/technology",
+        link: "/dashboard/technology"
       },
       {
         img: require("@/assets/img/subjects/philosophy.svg"),
         name: "Philosophy",
-        link: "/dashboard/philosophy",
+        link: "/dashboard/philosophy"
       },
       {
         img: require("@/assets/img/subjects/geographi.svg"),
         name: "Geographi",
-        link: "/dashboard/geographi",
+        link: "/dashboard/geographi"
       },
       {
         img: require("@/assets/img/subjects/language.svg"),
         name: "French",
-        link: "/dashboard/language",
-      },
+        link: "/dashboard/language"
+      }
     ],
     complatedTasks: [
       {
         complated: 35,
-        max: 56,
+        max: 56
       },
       {
         complated: 12,
-        max: 20,
+        max: 20
       },
       {
         complated: 0,
-        max: 10,
+        max: 10
       },
       {
         complated: 1,
-        max: 4,
-      },
+        max: 4
+      }
     ],
+    successList: [
+      {
+        img: require("@/assets/img/home/video-placeholder-1.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-1.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-1.jpg"),
+        title: "Are you studying and still can’t do it?"
+      }
+    ],
+    showList: [
+      {
+        img: require("@/assets/img/home/video-placeholder-2.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-2.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-2.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-2.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-2.jpg"),
+        title: "Are you studying and still can’t do it?"
+      },
+      {
+        img: require("@/assets/img/home/video-placeholder-2.jpg"),
+        title: "Are you studying and still can’t do it?"
+      }
+    ]
   }),
-  components: {
-    UseApp,
+  methods: {
+    sidebarActive(e) {
+      let sidebarElements = document.getElementsByClassName("sidebar__item");
+
+      sidebarElements.forEach(element => {
+        if (element.classList.contains("_active")) {
+          element.classList.remove("_active");
+        }
+      });
+
+      e.target.classList.add("_active");
+    }
   },
+  components: {
+    UseApp
+  }
 };
 </script>
