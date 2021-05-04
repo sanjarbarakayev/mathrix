44c341361566970dcc7c3c44b9ddc336ff816e5c
Пользователь <genius.s.1799@gmail.com>
2021-05-04 12:02:35 +0500

    init: start chapters page

---
+++ pages/chapters.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="chapters">
+    <el-breadcrumb separator-class="el-icon-d-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">Dashboard</el-breadcrumb-item>
+      <el-breadcrumb-item>Philosophy</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
