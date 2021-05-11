661b133a3d0a50174d101f7e02092866e16e4993
Пользователь <genius.s.1799@gmail.com>
2021-05-11 13:01:57 +0500

    fix: add exam-modal to separate page

---
+++ pages/exam-modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- Exam modal -->
+  <div
+    class="exam-modal container mx-auto py-6 w-screen h-screen"
+    v-if="!activeStep"
+  >
+    <div class="exam-modal__header text-right">
+      <button>
+        <img
+          class="exam-modal__close-btn"
+          src="@/assets/img/icons/close-icon.svg"
+          alt="close-icon"
+        />
+      </button>
+    </div>
+    <div
+      class="exam-modal__main flex flex-col items-center justify-center h-full"
+    >
+      <img src="@/assets/img/logo/success-logo.svg" alt="happy-mathrix" />
+      <h2 class="exam-modal__title mt-8 mb-4">The complex plane</h2>
+      <p class="exam-modal__subtitle">
+        You are about to start a 30 minute questioning. Put yourself in
+        condition and click on the Start button as soon as you are ready!
+      </p>
+      <button class="exam-modal__btn m-btn-primary mt-8" @click="startExam">
+        Let’s Go
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "clear",
+  methods: {
+    startExam() {
+      this.$router.push("/exam");
+    }
+  }
+};
+</script>
--- pages/exam.vue
+++ pages/exam.vue
@@ -21,37 +21,7 @@
             :stroke-width="10"
           ></el-progress>
         </div>
-        <!-- Exam modal -->
-        <div
-          class="exam-modal container mx-auto py-6 fixed inset-0 bg-white w-screen h-screen"
-          v-if="!activeStep"
-        >
-          <div class="exam-modal__header text-right">
-            <button>
-              <img
-                class="exam-modal__close-btn"
-                src="@/assets/img/icons/close-icon.svg"
-                alt="close-icon"
-              />
-            </button>
-          </div>
-          <div
-            class="exam-modal__main flex flex-col items-center justify-center h-full"
-          >
-            <img src="@/assets/img/logo/success-logo.svg" alt="happy-mathrix" />
-            <h2 class="exam-modal__title mt-8 mb-4">The complex plane</h2>
-            <p class="exam-modal__subtitle">
-              You are about to start a 30 minute questioning. Put yourself in
-              condition and click on the Start button as soon as you are ready!
-            </p>
-            <button
-              class="exam-modal__btn m-btn-primary mt-8"
-              @click="startExam"
-            >
-              Let’s Go
-            </button>
-          </div>
-        </div>
+
         <!-- Exam timer -->
         <div class="exam-timer flex items-center mb-4">
           <img src="@/assets/img/icons/time-icon.svg" alt="time-icon" />
@@ -194,13 +164,13 @@ export default {
     ]
   }),
   methods: {
-    startExam() {
-      this.activeStep = 1;
-    },
     nextStep() {
       this.activeStep++;
       this.percentage += 25;
     }
+  },
+  mounted() {
+    this.activeStep = 1;
   }
 };
 </script>
